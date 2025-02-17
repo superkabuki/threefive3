9a16fc96713581af4a6db824f6453fbc0d6e3ca2
Adrian of Doom <a@iodisco.com>
2025-02-17 02:56:47 -0500

    Update __init__.py

--- threefive3/__init__.py
+++ threefive3/__init__.py
@@ -3,6 +3,7 @@ scte35.__init__.py
 """
 
 from .spare import print2
+
 from .cue import Cue
 from .section import SpliceInfoSection
 from .segment import Segment
@@ -10,18 +11,36 @@ from .stream import Stream
 from .version import version
 
 from .commands import (
+    SpliceCommand,
     TimeSignal,
     SpliceInsert,
     SpliceNull,
     PrivateCommand,
     BandwidthReservation,
+    command_map,
 )
 
 from .descriptors import (
+    k_by_v,
     AvailDescriptor,
     DVBDASDescriptor,
     DtmfDescriptor,
     SegmentationDescriptor,
     SpliceDescriptor,
     TimeDescriptor,
+    descriptor_map,
+
 )
+
+from .upids import(
+    Upid,
+    NoUpid,
+    AirId,
+    Atsc,
+    Eidr,
+    Isan,
+    Mid,
+    Mpu,
+    Umid,
+    upid_map,
+ )   
