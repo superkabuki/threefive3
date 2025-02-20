be816fea174d4e8141c7e8efd10d970df7dde397
Adrian of Doom <a@iodisco.com>
2025-02-19 19:19:13 -0500

    upid_map messages

--- threefive3/upids.py
+++ threefive3/upids.py
@@ -320,6 +320,7 @@ class Mpu(Upid):
         """
         decode MPU Upids
         """
+
         self.upid_value = {
             "format_identifier": self.bitbin.as_charset(32),
             "private_data": self.bitbin.as_hex(self.bit_length - 32),
@@ -381,11 +382,11 @@ class Umid(Upid):
 # segmentation_upid_type : [name, class, length]
 upid_map = {
     0x00: ["No UPID", NoUpid, 0],
-    0x01: ["Type 0x01 is deprecated. Use MPU, type 0x0C, .", Upid, False],
-    0x02: ["Type 0x02 is deprecated. Use AdID, type 0x03 .", Upid, 8],
+    0x01: ["Type 0x01 is deprecated, use MPU type 0x0C", Upid, False],
+    0x02: ["Type 0x02 is deprecated, use AdID type 0x03", Upid, 8],
     0x03: ["AdID", Upid, 12],
     0x04: ["UMID", Umid, 32],
-    0x05: ["Type 0x05 is deprecated. Use ISAN.,type 0x06, ", Isan, 8],
+    0x05: ["Type 0x05 is deprecated, use ISAN type 0x06", Isan, 8],
     0x06: ["ISAN", Isan, 12],
     0x07: ["TID", Upid, 12],
     0x08: ["AiringID", AirId, 8],
