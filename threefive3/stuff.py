112fb9b2f854b7d868ac5f5427b2e7eb3365cf7b
Adrian of Doom <a@iodisco.com>
2025-02-21 21:23:58 -0500

    atohif() and iso8601()

--- threefive3/stuff.py
+++ threefive3/stuff.py
@@ -1,7 +1,10 @@
 """
 stuff.py functions and such common to threefive3.
+
+print2, atohif, and iso8601
 """
 
+import datetime
 from sys import stderr
 
 
@@ -12,7 +15,7 @@ def print2(gonzo=b""):
     print(gonzo, file=stderr, flush=True)
 
 
-def atoif(value):
+def atohif(value):
     """
     atoif converts ascii to (int|float)
     """
@@ -21,6 +24,24 @@ def atoif(value):
         value = value.replace(',','')
         if '.' in value:
             value = float(value)
+        elif '0x' in value.lower():
+            value = int(value,16)
         elif  value.isdigit():
             value = int(value)
     return value
+
+
+
+def iso8601():
+    """
+    return UTC time in iso8601 format.
+
+    '2023-05-11T15:55:51.'
+
+    """
+    return f"{datetime.datetime.utcnow().isoformat()[:-4]}Z "
+
+
+
+
+
