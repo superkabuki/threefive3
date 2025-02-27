abbcd487a486a5b101bfa4a18dcd8b5860b200d9
Adrian of Doom <a@iodisco.com>
2025-02-27 08:00:38 -0500

    added mk_splice_insert, mk_time_signal and SuperKabuki

--- threefive3/__init__.py
+++ threefive3/__init__.py
@@ -1,14 +1,16 @@
 """
-scte35.__init__.py
+threefive3.__init__.py
 """
 
 from .cue import Cue
+from .encode import mk_splice_null, mk_time_signal, mk_splice_insert
 from .hls import HlsParser
 from .hlstags import TagParser
 from .section import SpliceInfoSection
 from .segment import Segment
 from .stream import Stream
-from .stuff import print2, iso8601, atohif
+from .stuff import print2, iso8601, atohif,red,blue
+from .superkabuki import SuperKabuki
 from .version import version
 
 from .commands import (
