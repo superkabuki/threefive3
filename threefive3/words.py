aa73697d8bf76ef719e1abb7b5f663705b29ca8f
Adrian <a@iodisco.com>
2025-03-07 07:08:08 -0500

    Add files via upload

---
+++ threefive3/words.py
@@ -0,0 +1,44 @@
+"""
+threefive.words words.py
+lowercase constant word names for numbers and punctuation.
+
+"""
+
+
+# Numbers
+minusone = -0x1
+zero = 0x0
+one = 0x1
+two = 0x2
+three = 0x3
+four = 0x4
+five = 0x5
+six = 0x6
+seven = 0x7
+eight = 0x8
+nine = 0x9
+ten = 0xa
+eleven = 0xb
+twelve = 0xc
+thirteen = 0xd
+fourteen = 0xe
+fifteen = 0xf
+sixteen = 0x10
+twentyfour = 0x18
+thirtytwo = 0x20
+twofiftyfive = 0xff
+twofiftysix = 0x100
+
+
+# Quotes
+dblquote = '"'
+quote = "'"
+nothing = ""
+
+
+# Other
+comma = ","
+colon = ":"
+equalsign = "="
+octothorpe = "#"
+space = " "
