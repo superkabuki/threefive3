e01d13f8a05a5dd9ab1bab69324ec8affafc7427
Adrian <a@iodisco.com>
2025-03-07 17:53:04 -0500

    black

--- threefive3/words.py
+++ threefive3/words.py
@@ -4,7 +4,6 @@ lowercase constant word names for numbers and punctuation.
 
 """
 
-
 # Numbers
 minusone = -0x1
 zero = 0x0
@@ -17,16 +16,16 @@ six = 0x6
 seven = 0x7
 eight = 0x8
 nine = 0x9
-ten = 0xa
-eleven = 0xb
-twelve = 0xc
-thirteen = 0xd
-fourteen = 0xe
-fifteen = 0xf
+ten = 0xA
+eleven = 0xB
+twelve = 0xC
+thirteen = 0xD
+fourteen = 0xE
+fifteen = 0xF
 sixteen = 0x10
 twentyfour = 0x18
 thirtytwo = 0x20
-twofiftyfive = 0xff
+twofiftyfive = 0xFF
 twofiftysix = 0x100
 
 
